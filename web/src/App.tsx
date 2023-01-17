ec9779177cd4c9fe32cabaeee9e72558bd3304b3
VMarsola <vinicius.marasola2@gmail.com>
2023-01-17 15:42:50 -0300

    init web and back

---
+++ web/src/App.tsx
@@ -0,0 +1,19 @@
+import "./styles/global.css";
+
+import Habits from "./components/habits";
+
+function App() {
+  return (
+    <>
+      <Habits />
+      <Habits />
+      <Habits />
+      <Habits />
+      <Habits />
+      <Habits />
+      <Habits />
+    </>
+  );
+}
+
+export default App;
---
+++ web/src/components/habits/index.tsx
@@ -0,0 +1,7 @@
+export default function Habits() {
+  return (
+    <div className="bg-slate-800 text-white w-10 h-10 rounded m-2 flex items-center justify-center">
+      ih
+    </div>
+  );
+}
